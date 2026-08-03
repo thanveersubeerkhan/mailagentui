cd8e50820ec4312f75ab2acf258048d03bf8f850
thanveer <thanveer21cs52@gmail.com>
2026-08-03 11:45:05 +0530

    feat: implement PlatformPage for email management with comparison capabilities and external API integration

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/platform-api': {
-        target: 'https://portaldev.mawarid.com.sa:6080',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
 })
